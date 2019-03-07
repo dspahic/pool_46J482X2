79160dfb10b08e02027a32f69212f3a2d6c30cd3
Damir Spahic <dspahic85@gmail.com>
2019-03-07 13:10:47 +0100

    simple routing based on login status

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,33 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Login from './containers/Login';
+import Welcome from './containers/Welcome';
+
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isSignedIn: false,
+    }
+    this.onLogin = this.onLogin.bind(this);
+    this.onLogout = this.onLogout.bind(this);
+  }
+  onLogin() {
+    this.setState({ isSignedIn: true })
+  }
+
+  onLogout() {
+    this.setState({ isSignedIn: false })
+  }
   render() {
     return (
       <div className="App">
+        {!this.state.isSignedIn ?
+          <Login onLogin={this.onLogin} />
+          : <Welcome onLogout={this.onLogout} />}
       </div>
+
     );
   }
 }
