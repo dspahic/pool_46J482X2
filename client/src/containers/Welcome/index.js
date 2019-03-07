15ddd586c900428554d8bcf759f989d9cf04856c
Damir Spahic <dspahic85@gmail.com>
2019-03-07 13:07:34 +0100

    add welcome screen placeholder

---
+++ client/src/containers/Welcome/index.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './style.css';
+
+class App extends Component {
+  render() {
+    return (
+      <span>placeholder</span>
+    );
+  }
+}
+
+export default App;
